c56c1336ebb07ba7fd90738e342a9be5aae1672d
Stefan Alfbo <stefan@alfbo.se>
2026-08-15 18:07:24 +0200

    Add random selection function to Selection module

--- src/GeneticAlgorithms/Selection.fs
+++ src/GeneticAlgorithms/Selection.fs
@@ -5,6 +5,11 @@ module Selection =
     let elite (population: Chromosome<'Gene> array) (n: int) =
         population |> Array.take n
 
+    let random (population: Chromosome<'Gene> array) (n: int) =
+        population
+        |> Array.sortBy (fun _ -> System.Random.Shared.Next())
+        |> Array.take n
+
     let select (opts: Options<'Gene>) (population: Chromosome<'Gene> array) =
         let n = int (System.Math.Round(float population.Length * opts.selection_rate))
         let n = if n % 2 = 0 then n else n + 1
